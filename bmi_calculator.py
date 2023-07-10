c5028f2df016fcb14328a5cea6d9b7e8deca0186
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-07-10 13:53:15 +0300

    Delete bmi_calculator.py

--- bmi_calculator.py
+++
@@ -1,6 +0,0 @@
-def bmi(weight, height):
-    bmi = weight / height ** 2
-    if bmi <= 18.5: return "Underweight"
-    if bmi <= 25.0: return "Normal"
-    if bmi <= 30.0: return "Overweight"
-    if bmi > 30.0: return "Obese"
