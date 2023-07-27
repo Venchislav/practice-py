4e2fe1f835fb0afccf361874b8158b77733f8337
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-07-27 20:40:06 +0300

    Add files via upload

---
+++ codewars-14/count-lower.py
@@ -0,0 +1,2 @@
+def lowercase_count(strng):
+    return len([i for i in list(strng) if i.islower()])
---
+++ codewars-14/uefa.py
@@ -0,0 +1,5 @@
+def uefa_euro_2016(teams, scores):
+    if scores[0] != scores[1]:
+        return f"At match {teams[0]} - {teams[1]}, {teams[scores.index(max(scores))]} won!"
+    else: return f"At match {teams[0]} - {teams[1]}, teams played draw."
+    
