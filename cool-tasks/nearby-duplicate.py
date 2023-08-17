fe95ebfcc58a2b22575fc5b9593a8158f4ba9bc9
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-08-17 11:44:47 +0300

    Add files via upload

---
+++ cool-tasks/nearby-duplicate.py
@@ -0,0 +1,7 @@
+def containsNearbyDuplicate(nums, k):
+    dic = {}
+    for i, v in enumerate(nums):
+        if v in dic and i - dic[v] <= k:
+            return True
+        dic[v] = i
+    return False
