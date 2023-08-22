37547df7b40655541342644bdca56b753296f8d0
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-08-22 17:21:43 +0300

    Update 3sum.py

--- cool-tasks/3sum.py
+++ cool-tasks/3sum.py
@@ -2,6 +2,7 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = set()
         n, p, z = [], [], []
+        
         for i in nums:
             if i > 0:
                 p.append(i)
