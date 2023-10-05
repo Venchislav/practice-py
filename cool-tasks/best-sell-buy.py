f2520e651909f598a92fee022acbe6043714eec5
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-10-05 19:22:19 +0300

    It's more efficient and beautiful solution now🌸
    Better performance

--- cool-tasks/best-sell-buy.py
+++ cool-tasks/best-sell-buy.py
@@ -1,11 +1,15 @@
 def max_profit(prices):
-    l, r = 0, 1
-    max_prof = 0
-    while r < len(prices):
-        prof = prices[r] - prices[l]
-        if prices[l] > prices[r]:
-            l = r
+        n = len(prices)
+        if not prices or n < 2:
+            return 0
         else:
-            max_prof = max(prof, max_prof)
-        r += 1
-    return max_prof
+            max_profit = 0
+            min_price = prices[0]
+
+            for i in range(1, n):
+                if prices[i] < min_price:
+                    min_price = prices[i]
+                else:
+                    c_profit = prices[i] - min_price
+                    max_profit = max(max_profit, c_profit)
+        return max_profit
