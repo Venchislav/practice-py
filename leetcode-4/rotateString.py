3d2bcbe1a046151dcf1fccb2ac3f5ae055f36d9b
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-13 22:06:43 +0300

    Create rotateString.py

---
+++ leetcode-4/rotateString.py
@@ -0,0 +1,9 @@
+def rotateString(self, s, goal):
+    s_ = list(s)
+    for _ in range(len(s)):
+        s_.append(s_[0])
+        s_ = s_[1:]
+        print(s_)
+        if ''.join(s_) == goal:
+            return True
+    return False
