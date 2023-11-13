525b5e0b57f6facfcf3385dd2bc53f3cbd3574d4
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-13 22:06:16 +0300

    Delete leetcode-4/kth_distinct_elem.py

--- leetcode-4/kth_distinct_elem.py
+++
@@ -1,8 +0,0 @@
-s_ = list(s)
-    for _ in range(len(s)):
-        s_.append(s_[0])
-        s_ = s_[1:]
-        print(s_)
-        if ''.join(s_) == goal:
-            return True
-    return False
