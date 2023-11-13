55849aff57a3dcc461bf4cfb759ad0df4d243e2e
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-13 22:12:33 +0300

    Any program is only as good as it is useful © Linus Torvalds🐧

--- leetcode-4/rotateString.py
+++ leetcode-4/rotateString.py
@@ -7,3 +7,9 @@ def rotateString(s, goal):
         if ''.join(s_) == goal:
             return True
     return False
+
+
+print(rotateString("abcde", "abced"))
+# Bruh... I made too much commits in here just because of my absent-mindedness😪
+
+# P.S This code is not the best solution, but it's mine and I made it with my little hands and it performs quiet well (32ms; 13.18MB)
