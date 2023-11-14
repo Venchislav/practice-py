c1f46162df102b5fefc1841bce9ab4e7f0b32d59
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-14 21:14:16 +0300

    Duck🦆

--- leetcode-4/most_frequent_even.py
+++ leetcode-4/most_frequent_even.py
@@ -10,3 +10,21 @@ print(mostFrequentEven(a))
 # well
 # my solution is not 100% correct for leetcode...
 # so there's another solution
+
+class Solution(object):
+    def mostFrequentEven(self, nums):
+        seen = {}
+        for item in nums:
+            if item % 2 ==0:
+                seen[item] = 1 if item not in seen else seen[item] + 1
+        maxx = 0    
+        output = -1        
+        for num, count in seen.items():
+            if count > maxx:
+                maxx, output = count, num
+            elif count == maxx:
+                output = min(num,output)
+        return output
+
+
+        
