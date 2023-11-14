d954a6ac2427e6437454c4cd29018869f7ab1c90
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-14 21:12:15 +0300

    Create most_frequent_even.py

---
+++ leetcode-4/most_frequent_even.py
@@ -0,0 +1,9 @@
+def mostFrequentEven(nums):
+    nums_ = [i for i in nums if i % 2 == 0]
+    if len(nums_) == 0:
+        return -1
+    
+    return max(nums_, key=nums_.count) if len(set(nums_)) != len(nums_) else nums_[0]
+
+a = [1, 22, 2, 3, 3, 3, 2]
+print(mostFrequentEven(a))
