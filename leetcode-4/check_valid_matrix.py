188145b669e230bcf877e8e9924f6214234f3b41
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-21 14:01:45 +0300

    I fixed

--- leetcode-4/check_valid_matrix.py
+++ leetcode-4/check_valid_matrix.py
@@ -2,24 +2,25 @@ def checkValid(matrix):
         vertical_test = 0
         horizontal_test = 0
         matrix = np.array(matrix)
-        
+
         for i in range(matrix.shape[-1]):
             if sorted(matrix[:,i]) != list(range(1, matrix.shape[1] + 1)):
                 return False
             vertical_test += 1
-            
+
         for i in range(matrix.shape[0]):
             if sorted(matrix[i, :]) != list(range(1, matrix.shape[0] + 1)):
                 return False
             horizontal_test += 1
-            
+
         return (vertical_test == len(matrix)) and (horizontal_test == len(matrix))
-            
+
         for i in range(matrix.shape[0]):
             if sorted(matrix[:,i]) != list(range(1, len(matrix) + 1)):
                 return False
             horizontal_test += 1
-        return (vertical_test == len(matrix)) and (horizontal_test == len(matrix))
+        return matrix.shape() == [horizontal_test, vertical_test]
+
 
 
 # Too complex, too bad, but it's mine
