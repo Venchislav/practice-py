6ca1470af0b761458f2b9d62d32ff6d185d2d143
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-11-26 16:55:51 +0300

    Create row_max_ones.py

---
+++ leetcode-4/row_max_ones.py
@@ -0,0 +1,12 @@
+def rowAndMaximumOnes(mat):
+    max_r = 0
+    max_i = 0
+    for i in range(len(mat)):
+        if sum(mat[i]) == max_r:
+            max_r = max_r
+        else:
+            max_r = max(max_r, sum(mat[i]))
+            if max_r == sum(mat[i]):
+                max_i = i
+
+    return [max_i, max_r]
