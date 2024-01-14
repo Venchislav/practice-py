ee675a8082b6532e0728e0a742b0110376bca400
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-01-14 17:38:32 +0300

    Update missing_number.py

--- leetcode-4/missing_number.py
+++ leetcode-4/missing_number.py
@@ -1,5 +1,5 @@
 def missingNumber(self, nums: List[int]) -> int:
-  for num in range(0, max(nums) + 2):
+  for num in range(min(nums), max(nums) + 2):
     if not num in nums:
       return num
   return -1
